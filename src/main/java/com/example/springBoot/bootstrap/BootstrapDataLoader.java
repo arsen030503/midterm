6a7f253824715f7ef19a7c922018fc19ca36e56f
malikovadi <malikovadi21@gmail.com>
2024-03-23 22:48:51 +0600

    class for bootstrap data

---
+++ src/main/java/com/example/springBoot/bootstrap/BootstrapDataLoader.java
@@ -0,0 +1,14 @@
+package com.example.springBoot.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapDataLoader implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Simply return without performing any operations
+        return;
+    }
+}
