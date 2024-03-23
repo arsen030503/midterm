1297ba761cb2e2c334f0edbcef53e72c9bb8e258
malikovadi <malikovadi21@gmail.com>
2024-03-23 22:48:13 +0600

    developing entities

---
+++ src/main/java/com/example/springBoot/entities/Task.java
@@ -0,0 +1,23 @@
+package com.example.springBoot.entities;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
+
